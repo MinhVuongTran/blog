c600f83be1b33cb870859a17251bb625212351f5
MinhVuongTran <minhvuongtran9x@gmail.com>
2022-08-29 21:11:35 +0700

    [CRUD] Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,18 +1,20 @@
 import mongoose from "mongoose";
 import { Course } from "../models/Course.js";
+import { multipleMongooseToObject } from "../../util/mongoose.js";
+
 const course = mongoose.model("Course", Course);
 export class SiteController {
     // [GET] /
-    index(req, res) {
-        course.find({}, (err, courses) => {
-            if (!err) {
-                res.json(courses);
-            } else {
-                res.status(400).json({
-                    error: "error",
+    index(req, res, next) {
+        course
+            .find({})
+            .then((courses) => {
+                res.render("home", {
+                    // C1
+                    courses: multipleMongooseToObject(courses),
                 });
-            }
-        });
+            })
+            .catch(next);
         // res.render("home");
     }
 
