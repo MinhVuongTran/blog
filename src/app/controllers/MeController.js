981ee503d214a3c86385d87222e041428270b90f
MinhVuongTran <minhvuongtran9x@gmail.com>
2022-09-02 22:18:47 +0700

    Deleted count documents

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -6,14 +6,28 @@ const course = mongoose.model("Course", Course);
 export class MeController {
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        course
-            .find()
-            .then((courses) =>
-                res.render("me/stored-courses", {
-                    courses: multipleMongooseToObject(courses),
-                })
-            )
-            .catch(next);
+        Promise.all([course.find(), course.countDocumentsDeleted()]).then(([courses, deletedCount]) => {
+            res.render("me/stored-courses", {
+                deletedCount,
+                courses: multipleMongooseToObject(courses),
+            });
+        });
+        //     .catch(() => {})
+        // course
+        //     .countDocumentsDeleted()
+        //     .then((deletedCount) => {
+        //         console.log(deletedCount);
+        //     })
+        //     .catch(() => {});
+
+        // course
+        //     .find()
+        //     .then((courses) =>
+        //         res.render("me/stored-courses", {
+        //             courses: multipleMongooseToObject(courses),
+        //         })
+        //     )
+        //     .catch(next);
     }
 
     // [GET] /me/trash/courses
